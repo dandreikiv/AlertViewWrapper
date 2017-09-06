52d0f79382fd3511b5fe04c62c3ab45ea8ff1dea
Dmytro Andreikiv <dmytro.andreikiv@philips.com>
2017-09-06 20:10:14 +0200

    Added comments.

--- AlertViewWrapper/AlertWrapper.swift
+++ AlertViewWrapper/AlertWrapper.swift
@@ -8,10 +8,24 @@
 
 import UIKit
 
+/**
+ The protocol to which a view controller should conform, in order to be abot to 
+ present an alert view via AlertWrapper.
+ */
 protocol AlertPresentable {
+	
+	/**
+	 The method redirects an action of alert view creation and presentation to a
+	 view controller.
+	*/
 	func showAlert(title: String?, message: String?, actions:[UIAlertAction], completion: (() -> Void)? );
 }
 
+/**
+ The structure is a wrapper for an UIAlertView and is being used in the Presenter.
+ As according to VIPER architecture the presenter should contain only plain objects 
+ and shouldn't know anything about UI.
+ */
 struct AlertAction {
 	let title: String?
 	let action: (() -> Void)?
