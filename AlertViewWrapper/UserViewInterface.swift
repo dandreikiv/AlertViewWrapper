235f197bb6a548bfc54b54e5a8efebd3f1128188
Dmytro Andreikiv <dmytro.andreikiv@philips.com>
2017-09-06 19:49:56 +0200

    Added comments.

--- AlertViewWrapper/UserViewInterface.swift
+++ AlertViewWrapper/UserViewInterface.swift
@@ -8,6 +8,17 @@
 
 import Foundation
 
+/**
+ The protocol defines number of methods by which Presenter communicates back
+ to a view.
+ */
 protocol UserViewInterface {
+	
+	/**
+	 Notifies a view that a list of users should be updated with given array 
+	 of users.
+	
+	 - Parameter users: a list of users stored in data storage.
+	 */
 	func reloadList(users:[User])
 }
